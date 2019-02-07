ca522c007595a9e908570cf68fc0cf895e234c9a
Aram <aramsyper@gmail.com>
2019-02-07 18:32:38 +0300

    last izmeneniya

--- PPLessonRKSI/Практическая1/main.js
+++ PPLessonRKSI/Практическая1/main.js
@@ -5,9 +5,10 @@
 
 let input = document.querySelector('input');
 let button = document.querySelector('button');
-
+let section = document.querySelector('section');
 
 button.addEventListener('click',()=>{
+    section.innerHTML = '';
     let inputVal = input.value;
     inputVal = inputVal.replace(/ /g, "+");
 const loadImage = (url) => {
@@ -35,37 +36,36 @@ const embeded = (url) => {
             console.log(movieInfo);
             movieInfo = movieInfo.Search;
             console.log(movieInfo);
-         
-            let allMoviesInfo = [];
-            for(let i = 0,len = movieInfo.length;i<len;i++){
-            let moviesInfo = [];
-            let movieError = movieInfo[i].Error;
-            let moviePoster = movieInfo[i].Poster;
-            let movieYear = movieInfo[i].Year;
-            let movieTitle = movieInfo[i].Title;
-            moviesInfo.push(moviePoster,movieYear, movieTitle,movieError);
-            allMoviesInfo.push(moviesInfo);
-            console.log(moviesInfo); 
-            console.log(allMoviesInfo);
-           }  
-                return allMoviesInfo;
+            if(movieInfo === undefined){
+                alert("Movie not found");
+            }else{
+                let allMoviesInfo = [];
+                for(let i = 0,len = movieInfo.length;i<len;i++){
+                    let moviesInfo = [];
+                    let moviePoster = movieInfo[i].Poster;
+                    let movieYear = movieInfo[i].Year;
+                    let movieTitle = movieInfo[i].Title;
+                    moviesInfo.push(moviePoster,movieYear, movieTitle);
+                    allMoviesInfo.push(moviesInfo);
+                    console.log(moviesInfo); 
+                    console.log(allMoviesInfo);
+            }  
+                    return allMoviesInfo;
+        }
         })
         .then(info=>{ 
             for(let i = 0,len = info.length;i<len;i++){
-                if(info[i][0] === undefined){
-                    alert(info[3]);
-                }else{
                     let img = document.createElement('img');
                     img.src=info[i][0];
                     let pYear = document.createElement('p');
                     let pTitle = document.createElement('p');
                     pYear.innerHTML = `Год выпуска: ${info[i][1]}`;
                     pTitle.innerHTML = `Название: ${info[i][2]}`;
-                    document.body.appendChild(pTitle); 
-                    document.body.appendChild(img); 
-                    document.body.appendChild(pYear); 
+                    section.appendChild(pTitle); 
+                    section.appendChild(img); 
+                    section.appendChild(pYear); 
                 }     
-            } 
+            
         })
         .catch(error =>{
             console.log(error);
@@ -74,6 +74,7 @@ const embeded = (url) => {
 Promise.all([
     embeded(`http://www.omdbapi.com/?s=${inputVal}&apikey=d5677312`),
 ]);
+input.value="";
 });
 
 // embeded('http://www.omdbapi.com/?t=Movie+43&apikey=d5677312');
